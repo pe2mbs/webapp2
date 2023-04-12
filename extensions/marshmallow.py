15acb86d923496d19e447c4c3882007c89b56033
Marc Bertens-Nguyen <marc.bertens@equensworldline.com>
2023-04-12 07:02:34 +0200

    Fix for the model class

--- extensions/marshmallow.py
+++ extensions/marshmallow.py
@@ -25,3 +25,5 @@ from flask_marshmallow import Marshmallow
 API.mm      = Marshmallow()
 if not hasattr( API.mm, 'SQLAlchemySchema' ):
     API.mm.SQLAlchemySchema = API.mm.ModelSchema
+if not hasattr( API.mm, 'ModelSchema' ):
+    API.mm.ModelSchema = API.mm.SQLAlchemySchema
